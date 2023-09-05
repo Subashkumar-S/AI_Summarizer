5c2c940a633624fac92a540c5a9383d5035f814f
Subashkumar-S <subashs2232@gmail.com>
2023-09-05 23:05:55 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import { logo } from "../assets "
+
+const Hero = () => {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+                <button type='button' onClick={() => window.open("")} className='black_btn'>
+                    Github
+                </button>
+            </nav>
+            <h1 className='head_text font-inter'>Summarize Articles with <br className='max-md:hidden' />
+                <span className='orange_gradient'>OpenAI GPT-4</span>
+            </h1>
+            <h2 className="desc font-satoshi">
+            Effortless Content Digests: Revolutionize Your Reading Experience with OpenAI Article Summarizer API
+            </h2>
+        </header>
+    )
+}
+
+export default Hero
